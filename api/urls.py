5b0767edd9565748bfdd54244c9a6f083ec91315
Mutalip <jokermt235@yandex.ru>
2021-04-19 15:32:01 +0600

    Adding serializer and Views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
-"""config URL Configuration
-
+"""api URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,10 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path , include
 
+from django.contrib import admin
+from django.urls import path
+from .views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('category/all/', CategoryListView.as_view()),
+    path('category/create/', CategoryCreateView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import  *
 
 # Create your views here.
+
+class CategoryListView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    queryset  = Category.objects.all()
+
+class CategoryCreateView(generics.CreateAPIView):
+    serializer_class = CategorySerializer
