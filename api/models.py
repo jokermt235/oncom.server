f4619f58a5d958bdfc99643ffd98d12ca1aa6452
Mutalip <jokermt235@yandex.ru>
2021-04-19 15:22:26 +0600

    Adding category model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Category(models.Model):
+    name     = models.CharField(unique=True , max_length=255)
+    desc     = models.CharField(max_length=255)
+    created  = models.DateField(default=date.today)
+    modified = models.DateField(default=date.today)
