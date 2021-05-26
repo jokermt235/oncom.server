0845f83c0f3e07e821d4db639c1c3e5a78c27a0a
root <root@244275.simplecloud.ru>
2021-05-26 07:23:48 +0300

    Update urls

--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-=======
 from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
->>>>>>> bef2b9378b24d05e4083d61ca9698a0f3588d401
 ]
